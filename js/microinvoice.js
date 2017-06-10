28fffba38da9a82d7d3e9a2f0e5700056fd685e3
Tom Carbon <tomcarbon@gmail.com>
2017-06-09 21:12:40 -0700

    Add files via upload
    changes made to v.276
    
    Pay Spot:
    
    1) Fixed the red 'reddit links' button
    
    2) new functionality: SoDogetip 'deposit' button

--- js/microinvoice.js
+++ js/microinvoice.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 /******************************************************************
-* 20161014 tcc
-* cakeday
+* 20170423 tcc
+* a wow sunday.
 ******************************************************************/
-//window.alert("Hello from the initialization portion of payspot.js. ");
+//window.alert("Hello from the initialization portion of microinvoice.js. ");
 var tx = coinjs.transaction();
 var tempstring;
 var work;
@@ -13,9 +13,8 @@ var work_unconfirmed_balance;
 var work_redeem_script;
 
 //$("#tsNewScreen01"   	).toggle("fast","swing");	// hide this at initialization time
-$("#psNewScreen01"  	).toggle("fast","swing");	// hide this at initialization time
-$("#psNewScreen02"  	).toggle("fast","swing");	// hide this at initialization time This is the for Mini Invoice  for the user to populate.
-$("#psNewScreen03"  	).toggle("fast","swing");	// hide this at initialization time This is the for SoDogeTip interface (amount)
+$("#MINewScreen01"  	).toggle("fast","swing");	// hide this at initialization time
+$("#MINewScreen02"  	).toggle("fast","swing");	// hide this at initialization time This is the for Mini Invoice  for the user to populate.
 
 /* user presses the Spend button, this opens up the Spend Dialog Box */ 
 /*$("#psTogBtn1").click(function(){       // SPEND
@@ -24,12 +23,12 @@ $("#psNewScreen03"  	).toggle("fast","swing");	// hide this at initialization ti
 */
 
 /* user clicks on the 'Quick Instructions' */
-$("#psQuickInstructions").click(function(){
-        $("#psNewScreen01").toggle();
+$("#MIQuickInstructions").click(function(){
+        $("#MINewScreen01").toggle();
 });
 
 
-var _getVerify = _get("payspot");
+var _getVerify = _get("microinvoice");
 if(_getVerify[0]){
 var tt0 = _getVerify[0];
 
@@ -37,9 +36,11 @@ var tt0 = _getVerify[0];
         console.info("_getVerify[0] = " + _getVerify[0]);
                     work_address = tt0.slice(0);         // the address
 
-               $("#psAddress").val(work_address);
+               $("#MIAddress").val(work_address);
 
-	makePaySpot();		
+	makeMicroInvoice();		
+
+        $("#MINewScreen02").toggle();
 
     setTimeout(function () {
 //        if (address) {
@@ -74,66 +75,23 @@ function _get(value) {
 });*/
 
 /* user clicks on Reddit (search) Link */
-$("#psRedditBtn").click(function(){
-var v1  = document.getElementById("psAddress").value;
-console.info("v1 = " + v1);
-	var tt0 = 'https://www.reddit.com/search?q=%22' + v1 + '%22&sort=new' ;
+$("#MIRedditBtn").click(function(){
+var v1  = document.getElementById("MIAddress").value;
+	var tt0 = 'https://www.reddit.com/search?q=%22' + document.location.origin+''+document.location.pathname+  '?payspot='+v1+'#payspot' + '%22';
 	window.open(tt0);
 });
 
-/* user clicks on Reddit xxxgetipbot loading Link */
-/* user clicks on Reddit sodogetip loading Link */
-$("#psRedditLoadBtn").click(function(){
-var v1  = document.getElementById("psAddress").value;
-var v2  = document.getElementById("psSoDogeTipAmt").value;
-//var tt0 = 'https://www.reddit.com/message/compose?to=sodogetip&subject=withdraw&message=%2Bwithdraw%20' + v1 + '%20ReplaceMeWithAmount%20doge verify';
-var tt0 = 'https://www.reddit.com/message/compose?to=sodogetip&subject=%2Bwithdraw&message=%2Bwithdraw%20' + v2 + '%20doge%20to%20' + v1;
-	window.open(tt0)
+/* user clicks on Reddit dogetipbot loading Link */
+$("#MIRedditLoadBtn").click(function(){
+var v1  = document.getElementById("MIAddress").value;
+var tt0 = 'https://www.reddit.com/message/compose?to=dogetipbot&subject=withdraw&message=%2Bwithdraw%20' + v1 + '%20ReplaceMeWithAmount%20doge verify';
+	window.open(tt0);
 });
 
-/* user clicks on "SoDogeTip" button to open the interface */
-$("#psSoDogeTipBtn").click(function(){
-var v1  = document.getElementById("psAddress").value;
-	console.info("You've clicked on the SoDogeTip Button!");
-        $("#psNewScreen03").toggle();
-});
-
-/* user clicks on "Create Micro Invoice" Button */
-$("#psMicroInvoiceBtn").click(function(){
-var v1  = document.getElementById("psAddress").value;
-	console.info("You've clicked on the Micro Invoice Button!");
-        $("#psNewScreen02").toggle();
-	alert("This part is UNDER CONSTRUCTION! Sorry. Come back later!");
-});
-
-/* user clicks on "Next" Button on the Create Mini Invoice Screen */
-$("#psMicroInvoiceNextBtn").click(function(){
-//var v1  = document.getElementById("psMI01").value;	// info
-var v2  = document.getElementById("psMI02").value;	// amount
-//var v3  = document.getElementById("psMI03").value;	// contact
-//var v4  = document.getElementById("psMI04").value;	// photo (IMGUR code)
-console.info("******************************************************************");
-console.info("* MICRO INVOICE CREATION!");
-console.info("******************************************************************");
-//console.info("Provided Info              = \n" + v1);
-console.info("Address to Pay To = \n" + work_address);
-console.info("Amount            = \n" + v2);
-//console.info("Contact Info      = \n" + v3);
-//console.info("IMGUR code        = \n" + v4);
-console.info("******************************************************************");
-var tt0 = document.location.origin+''+document.location.pathname+'?microinvoice=' + work_address + '#microinvoice';
-window.open(tt0);
-});
-
-/* user clicks on Next for generating the multisig */
-/*$("#newtsMultiSigAddress").click(function(){
-	$("#tsmultisigPubKeys").addClass("hidden");
-	makeTripleShibe();		// it'll have sorting 
-});*/
 
 /*************************************************
 **************************************************/
-function makePaySpot() {
+function makeMicroInvoice() {
 //var ps_address = document.getElementById("tsAddress").value;
 var keys = [];
 
@@ -141,57 +99,36 @@ var keys = [];
         if (work_address.length == 34) {
                 console.info("A single address and this is correct.");
  //               keys.push(ps_address);
-                $("#psAddress").val(work_address);
+                $("#MIAddress").val(work_address);
 	} else {
 		var tt1 = "ERROR: Address is not the expected size." + 
 			"Length: "   + work_address.length +  " (expected: 34)" ;
-		$("#psmultisigPubKeys").removeClass("hidden");
+		$("#MImultisigPubKeys").removeClass("hidden");
 		console.error(tt1);
 		alert(tt1);
 		return false;
 	}
 
-//	$("#psmultiSigData").removeClass("hidden");			// HAPPY PATH!
-//	$("#tsmultisigPubKeysAfter").removeClass("hidden");			// HAPPY PATH! but keep it hidden because we need it in the back.
-/*	$("#tsPubKeyAfter1").val(bs_pubKey1);		// these 3 display the readonly pubkeys..
-	$("#tsPubKeyAfter2").val(bs_pubKey2);	
-	$("#tsPubKeyAfter3").val(bs_pubKey3);		
-*/
 
         /***************************************
 	* Create the multisig, redeem etc here:
         ****************************************/
-/*
-        var multisig =  coinjs.pubkeys2MultisigAddress(keys, 2);
-                        $("#tsAddress").val(multisig['address']);
-                        $("#tsRedeemScript").val(multisig['redeemScript']);
-                        $("#tsRedeemScriptURL").val(document.location.origin+''+document.location.pathname+'?verify='+multisig['redeemScript']+'#verify');
-*/
 
 	/* here's the URL for this page. */
-	$("#psPageURL").val(document.location.origin+''+document.location.pathname+  '?payspot='+work_address+'#payspot');
+	$("#MIPageURL").val(document.location.origin+''+document.location.pathname+  '?microinvoice='+work_address+'#microinvoice');
 	
 	/*****************************************
 	* Get and display the balance(s)
 	*****************************************/
 	setTimeout(function () {
-		payspot_wallet_balance(work_address,"psBalance","psUCBalance");
+		microinvoice_wallet_balance(work_address,"MIBalance","MIUCBalance");
 	}, 750);
 }
 
-
-
-/* user clicks on the 'Quick Instructions' */
-/*
-$("#psQuickInstructions").click(function(){
-        $("#tsNewScreen01").toggle();
-});
-*/
-
 /* copy address */
-var copyTextareaBtn1 = document.querySelector('.js-pstextareacopybtn1');
+var copyTextareaBtn1 = document.querySelector('.js-MItextareacopybtn1');
 copyTextareaBtn1.addEventListener('click', function(event) {
-  var copyTextarea = document.querySelector('.js-pscopytextarea1');
+  var copyTextarea = document.querySelector('.js-MIcopytextarea1');
   copyTextarea.select();
 
   try {
@@ -204,9 +141,9 @@ copyTextareaBtn1.addEventListener('click', function(event) {
 });
 
 /* copy URL */
-var copyTextareaBtn2 = document.querySelector('.js-pstextareacopybtn2');
+var copyTextareaBtn2 = document.querySelector('.js-MItextareacopybtn2');
 copyTextareaBtn2.addEventListener('click', function(event) {
-  var copyTextarea = document.querySelector('.js-pscopytextarea2');
+  var copyTextarea = document.querySelector('.js-MIcopytextarea2');
   copyTextarea.select();
 
   try {
@@ -221,24 +158,23 @@ copyTextareaBtn2.addEventListener('click', function(event) {
 
 
 /* get from chain.so, then wait before displaying */
-/* tempola TCC20161011- get back and fix this whle function */
-function payspot_wallet_balance(multisig, urlName, UCurlName) {
+function microinvoice_wallet_balance(multisig, urlName, UCurlName) {
 var tt1;
 var tt2;
-console.info("payspot_wallet_balance. Input Address: " + multisig);
+console.info("microinvoice_wallet_balance. Input Address: " + multisig);
 	$.ajax ({
 		type: "GET",
 		url: "https://chain.so/api/v2/get_address_balance/DOGE/" + multisig,
 		dataType: "json",
 		error: function(data) {
 			tt1 = JSON.stringify(data, null, 4);
-			console.error("payspot_wallet_balance fail: %s",tt1);
+			console.error("microinvoice_wallet_balance fail: %s",tt1);
 		},
 		success: function(data) {
 			tt1 = JSON.stringify(data, null, 4);
 			work_balance = data.data.confirmed_balance;
 			work_unconfirmed_balance = data.data.unconfirmed_balance;
-			console.info("payspot_wallet_balance: " + tt1);
+			console.info("microinvoice_wallet_balance: " + tt1);
 		},
 		complete: function(data, status) {
 			var tempvar = "Unconfirmed Balance: " + work_unconfirmed_balance; // now set unconfirmed if non-0
